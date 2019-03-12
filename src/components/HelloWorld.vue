b710b88c11113d994763dd06b276569b1f3f3ef7
chengxiaolv <972987164@qq.com>
2019-03-12 19:53:44 +0800

    开始构建项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+      测试马上记账
+  </div>
+</template>
+
+<script>
+    export default {
+    data () {
+        return {
+            
+        }
+    }
+    }
+</script>
+
+<style scoped>
+
+
+</style>
