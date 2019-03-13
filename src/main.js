dbab05e24c21afdf5ab90d36bb6dce04dbc5c106
chengxiaolv <972987164@qq.com>
2019-03-13 11:23:37 +0800

    优化

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ Vue.prototype.utils = utils; //引入到原型上。
 import https from "./utils/axios.js"; //引入封装的axios  
 Vue.prototype.https = https; //引入到原型上。
 
-import * as filters from './utils/filters' //其中import * as filters from ‘./filters/filter.js’是导入filter.js文件中的所有过滤器
+import * as filters from './utils/filters' //引入所有过滤器
 Object.keys(filters).forEach(key => {
     Vue.filter(key, filters[key]);
 })
@@ -33,7 +33,7 @@ new Vue({
     components: { App },
     template: '<App/>',
 
-    created() { //刷新重新赋值。
+    created() { //刷新重新赋值，vuex
         var val = this.$store.getters.getCityFn;
         if (!val && localStorage.getItem("state") != 'null') {
             var num = localStorage.getItem("state");
